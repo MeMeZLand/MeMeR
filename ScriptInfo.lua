911543516231ae22a540b36a6b0ae6fec56dc54e
MeMeA <644770257@qq.com>
2026-01-23 01:20:07 +0800

    Update ScriptInfo.lua

--- ScriptInfo.lua
+++ ScriptInfo.lua
@@ -1,23 +1,24 @@
 return {
-    ScriptName = "MeMeR",
-    Version = "0.0.1a",
-    Announcement = "SkibidiToilet --MeMeZ DEV",
-    ExploitAnnouncement = "同志们好 --MeMeZ DEV",
-    Respects = {
-      ["7818860510"] = true,
-      ["7825858926"] = true
-    },
-    Blacklist = {
-      [111] = "Niggerlover",
-    },
-    Creators = {
-        {
-            "MEMEL",
-            "2372506489",
-        },
-        {
-            "MeMeA",
-            "2823797847",
-        }
-    }
+  ScriptName = "MeMeR",
+  Version = "0.0.1a",
+  UIVersion = "0.0.1",
+  Announcement = "SkibidiToilet --MeMeZ DEV",
+  ExploitAnnouncement = "同志们好 --MeMeZ DEV",
+  Respects = {
+    ["7818860510"] = true,
+    ["7825858926"] = true
+  },
+  Blacklist = {
+    [111] = "Niggerlover",
+  },
+  Creators = {
+      {
+          "MEMEL",
+          "2372506489",
+      },
+      {
+          "MeMeA",
+          "2823797847",
+      }
   }
+}
